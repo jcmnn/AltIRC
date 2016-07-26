fe70150828d886320942c434addaf96b2fc118f4
Altenius <jacobjm18@gmail.com>
2016-07-26 10:35:58 -0400

    Added command 'source'

--- AltIRC/plugins/misc.lua
+++ AltIRC/plugins/misc.lua
@@ -1,3 +1,7 @@
 registerCommand("echo", "misc_echo", function(conn, prefix, reply, args)
     reply(args)
 end)
+
+registerCommand("source", "misc_source", function(conn, prefix, reply, args)
+    reply("https://github.com/Altenius/AltIRC")
+end)
